5b3f8ab9c54d4f6167c5858fb0a4d8c8ff1989f9
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-05-09 11:10:58 -0500

    SearchBar used includes, filter and toLowerCase methods

---
+++ searchBar_react/search_bar/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import JSONDATA from './MOCK_DATA.json'
+import {useState} from 'react'
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <div className="App">
+      <input type="text" placeholder="Search..." onChange={ event => {setSearchTerm(event.target.value)}} />
+      {JSONDATA.filter((val)=>{
+        if (searchTerm == ""){
+          return val
+        } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+          return val
+        }
+      } ).map((val,key) =>{
+        return <div><p>{val.first_name}</p></div>
+      })}
+    </div>
+  );
+}
+
+export default App;
