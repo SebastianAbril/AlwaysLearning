5add738fa2e0267ec08d3135ec3d52cec468a384
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-05-31 15:49:11 -0500

    Regular Expression con link con excelente pagina, metodo test y exec

--- regular_expressions/index.js
+++ regular_expressions/index.js
@@ -1,1 +1,12 @@
-// RegEx = Regular expressions son una manera de buscar a trabes de un string.
+// RegEx = Regular expressions son una manera de buscar a traves de un string.
+// https://regexr.com/
+
+const string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer metus ligula efficitur ut porttitor sit amet, molestie ut lorem. Cras iaculis elementum ante. Quisque libero odio"
+
+
+let regularExpression = new RegExp("lorem", "ig");
+
+let regularExpression2 = /lorem/ig;
+
+console.log(regularExpression2.test(string));
+console.log(regularExpression2.exec(string));
