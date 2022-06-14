b746abcab08205e1a09eb273058a76d58a196516
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-06-14 15:17:23 -0500

    Created the project with react App

---
+++ javascript-ordering-func/src/components/SelectOrder/SelectOrder.jsx
@@ -0,0 +1,16 @@
+import './SelectOrder.css';
+const SelectOrder = () => {
+    return(
+        <select className="SelectOrder">
+            <option value="Ascending">Ascending</option>
+            <option value="Descending">Pepe</option>
+        </select>
+    )
+}
+
+export default SelectOrder;
+
+
+
+
+
